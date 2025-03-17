a807d6b7e50c6c8fd8755fc1cc8559d990e91d2c
analisisysoluciones <53999209+analisisysoluciones@users.noreply.github.com>
2025-03-16 23:58:33 -0600

    actualizar comrpas para pagos a credito, reportes de cuenta banco

--- adm/forms.py
+++ adm/forms.py
@@ -160,12 +160,13 @@ class SimbologiaForm(forms.ModelForm):
 class CompraEncForm(forms.ModelForm):
     class Meta:
         model = CompraEnc
-        fields = ['proveedor', 'fecha', 'orden_compra','folio_documento', 'proyecto', 'tipo', 'inventario', 'total']
+        fields = ['proveedor', 'fecha', 'orden_compra','folio_documento', 'proyecto', 'tipo', 'inventario', 'total','dias_credito']
         widgets = {
             'proveedor': forms.Select(attrs={'class': 'form-control select2'}),
             'proyecto': forms.Select(attrs={'class': 'form-control select2'}),  # Campo de proyecto como dropdown
             'tipo': forms.Select(attrs={'class': 'form-control select2'}),      # Campo de tipo como dropdown
             'fecha': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'dias_credito':forms.TextInput(attrs={'class': 'form-control'}),
             'orden_compra': forms.TextInput(attrs={'class': 'form-control'}),
             'folio_documento': forms.TextInput(attrs={'class': 'form-control'}),
             'inventario': forms.TextInput(attrs={'class': 'form-control'}),
